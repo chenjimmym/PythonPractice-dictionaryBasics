c4d3952c4d57ca336cd4239de9a48943847d0cae
Jimmy <chen.jimmym@gmail.com>
2017-10-03 14:58:34 -0700

    done

---
+++ dictionaryBasics.py
@@ -0,0 +1,5 @@
+myDictionary = {"name":"Jimmy","age":"22","COB":"China","favorite language":"Python"}
+print 'My name is', myDictionary['name']
+print 'My age is', myDictionary['age']
+print 'My country of birth is', myDictionary['COB']
+print 'My favorite language is', myDictionary['favorite language']
